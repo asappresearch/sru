9a4958644e34a2ed8c0f62441425bc955792f606
taolei87 <taolei@csail.mit.edu>
2017-09-07 16:52:50 -0400

    Update cuda_functional.py
    make num_layers=2 default.

--- cuda_functional.py
+++ cuda_functional.py
@@ -487,7 +487,7 @@ class SRUCell(nn.Module):
 
 
 class SRU(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, dropout=0, rnn_dropout=0,
+    def __init__(self, input_size, hidden_size, num_layers=2, dropout=0, rnn_dropout=0,
                 use_tanh=1, bidirectional=False):
         super(SRU, self).__init__()
         self.n_in = input_size
