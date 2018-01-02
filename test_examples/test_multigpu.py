21b532195207b5d75351a3cd0de127a6270b9513
Tao Lei <taolei@rosetta8.csail.mit.edu>
2018-01-02 15:55:05 -0500

    1

--- test_examples/test_multigpu.py
+++ test_examples/test_multigpu.py
@@ -4,31 +4,41 @@ import torch.nn as nn
 from torch.autograd import Variable
 from cuda_functional import SRU
 
+class Model(nn.Module):
+    def __init__(self, rnn):
+        super(Model, self).__init__()
+        self.rnn = rnn
+
+    def forward(self, x):
+        out, state = self.rnn(x)
+        return out[-1:]
+
 def run(x, model, N):
     start = time.time()
     for _ in range(N):
         h = model(x)
     torch.cuda.synchronize()
+    print (h.size())
     print ("{:.4}".format(time.time()-start))
 
-N = 1000
-input_size, hidden_size = 2048, 2048
+N = 100
+input_size, hidden_size = 256, 1024
 num_layers = 2
-batch_size = 256
-length = 32
+batch_size = 128*2
+length = 64
 
-x = Variable(torch.randn(length, batch_size, input_size).float())
+x = Variable(torch.randn(length, batch_size, input_size).float(), volatile=True)
 x = x.cuda()
 
 # single gpu
-rnn = SRU(input_size, hidden_size, num_layers)
+rnn = Model(SRU(input_size, hidden_size, num_layers))
 rnn.cuda()
 rnn(x)
 print ("Single gpu:")
 run(x, rnn, N)
 
 # multiple gpu
-rnn_2 = SRU(input_size, hidden_size, num_layers)
+rnn_2 = Model(SRU(input_size, hidden_size, num_layers))
 rnn_2 = nn.DataParallel(rnn_2, dim=1)
 rnn_2.cuda()
 rnn_2(x)
