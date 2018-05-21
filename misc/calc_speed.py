7053e01281232b62746e7e607c2bda520f8e58ab
Tao Lei <tao@asapp.com>
2018-05-21 19:58:37 +0000

    minor

--- misc/calc_speed.py
+++ misc/calc_speed.py
@@ -16,15 +16,19 @@ def reset_seed(seed=1234):
 
 def test_fast(L, B, N, train=False):
     reset_seed()
-    a = Variable(torch.randn(L, B, N).float().cuda())
+    a = Variable(torch.randn(L, B, N).float().cuda()*0.1)
     c = Variable(torch.zeros(B, N).float().cuda())
     cell = SRUCell(N, N, dropout=0, use_tanh=False).cuda()
     if train: cell.train()
     torch.cuda.synchronize()
     start = time.time()
+    tot = 0
     for i in range(T):
         out = cell(a, c)
-        out[0].sum().backward()
+        tot += out[0].data[-1,-1,-1]
+        if train:
+            cell.zero_grad()
+            out[0].mean().backward()
     torch.cuda.synchronize()
     print ("test_fast: {:.6f}".format(
         (time.time()-start)/T
@@ -32,15 +36,19 @@ def test_fast(L, B, N, train=False):
 
 def test_lstm(L, B, N, train=False):
     reset_seed()
-    a = Variable(torch.randn(L, B, N).float().cuda())
+    a = Variable(torch.randn(L, B, N).float().cuda()*0.1)
     h = Variable(torch.zeros(1, B, N).float().cuda())
     cell = nn.LSTM(N, N, dropout=0.0).cuda()
     if train: cell.train()
     torch.cuda.synchronize()
     start = time.time()
+    tot = 0
     for i in range(T):
         out = cell(a, (h,h))
-        out[0].sum().backward()
+        tot += out[0].data[-1,-1,-1]
+        if train:
+            cell.zero_grad()
+            out[0].mean().backward()
     torch.cuda.synchronize()
     print ("test_lstm: {:.6f}".format(
         (time.time()-start)/T
@@ -48,20 +56,24 @@ def test_lstm(L, B, N, train=False):
 
 def test_conv(L, B, N, k=2, train=False):
     reset_seed()
-    a = Variable(torch.randn(B, 1, L, N).float().cuda())
+    a = Variable(torch.randn(B, 1, L, N).float().cuda()*0.1)
     conv = nn.Conv2d(1, N, (k, N)).cuda()
     if train: conv.train()
     torch.cuda.synchronize()
     start = time.time()
+    tot = 0
     for i in range(T):
         out = conv(a)
-        out.sum().backward()
+        tot += out.data[-1,-1,-1,-1]
+        if train:
+            conv.zero_grad()
+            out.mean().backward()
     torch.cuda.synchronize()
     print (("test_conv: {:.6f}").format(
         (time.time()-start)/T
     ))
 
-def test(L, N, D, train=True):
+def test(L, N, D, train=False):
     print (L, N, D)
     test_fast(L, N, D, train)
     test_conv(L, N, D, k=2, train=train)
@@ -69,7 +81,6 @@ def test(L, N, D, train=True):
     test_lstm(L, N, D, train)
 
 if __name__=="__main__":
-    test(32, 32, 128)
     test(32, 32, 256)
-    test(128, 32, 256)
+    test(128, 32, 512)
 
