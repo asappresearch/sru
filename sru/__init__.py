9d03c7dec2b6570c63ba922ed8204ba25c5638e0
Christopher Fox <cdfox@asapp.com>
2018-08-30 18:38:38 -0400

    add package docstring, visible from python

--- sru/__init__.py
+++ sru/__init__.py
@@ -1,3 +1,9 @@
+"""
+sru - Simple Recurrent Unit
+
+sru provides a PyTorch implementation of the simple recurrent neural network cell described
+in "Simple Recurrent Units for Highly Parallelizable Recurrence."
+"""
 from .version import __version__
 try:
     from .sru_functional import *
