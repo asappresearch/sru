218116172e12546c3c9f3d0f3fb56f701f0d2536
Tao Lei <taolei@csail.mit.edu>
2019-08-25 00:20:38 -0400

    resolve atomicAdd(double*, double) error

--- sru/sru_cuda_kernel.cu
+++ sru/sru_cuda_kernel.cu
@@ -5,6 +5,24 @@
 
 namespace {
 
+//  https://stackoverflow.com/questions/37566987/cuda-atomicadd-for-doubles-definition-error
+#if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
+#else
+__device__ double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull =
+                             (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+    do {
+        assumed = old;
+        old = atomicCAS(address_as_ull, assumed,
+                    __double_as_longlong(val +
+                    __longlong_as_double(assumed)));
+    } while (assumed != old);
+    return __longlong_as_double(old);
+}
+#endif
+
 template <typename scalar_t>
 __forceinline__ __device__ scalar_t sigmoidf(scalar_t x) {
     return 1.0 / (1.0 + exp(-x));
