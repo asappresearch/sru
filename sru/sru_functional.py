ae990369d67563acf35f80b80a0802963b622c1c
Hugh Perkins <hp@asapp.com>
2019-09-18 11:52:19 -0400

    add some doc, fix that sru expects sequence lenght first

--- sru/sru_functional.py
+++ sru/sru_functional.py
@@ -542,13 +542,25 @@ class SRU(nn.Module):
         """
         Feeds `input` forward through `num_layers` `SRUCell`s, where `num_layers`
         is a parameter on the constructor of this class.
+
+        parameters:
+        - input (FloatTensor): (sequence_length, batch_size, input_size)
+        - c0 (FloatTensor): (num_layers, batch_size, hidden_size * num_directions)
+        - mask_pad (ByteTensor): (sequence_length, batch_size): set to 1 to ignore the value at that position
+
+        input can be packed, which will lead to worse execution speed, but is compatible with many usages
+        of nn.RNN.
+
+        returns:
+        - prevx (FloatTensor): output, (sequence_length, batch_size, hidden_size)
+        - lstc (FloatTensor): state
         """
 
         # unpack packed, if input is packed. packing and then unpacking will be slower than not packing
-        # at all, but makes SRU usage compatible with nn.LSTM usage
+        # at all, but makes SRU usage compatible with nn.RNN usage
         input_packed = isinstance(input, nn.utils.rnn.PackedSequence)
         if input_packed:
-            input, lengths = nn.utils.rnn.pad_packed_sequence(input, batch_first=True)
+            input, lengths = nn.utils.rnn.pad_packed_sequence(input)
             max_length = lengths.max().item()
             mask_pad = torch.ByteTensor([[0] * l + [1] * (max_length - l) for l in lengths.tolist()]).to(input.device).transpose(0, 1).contiguous()
 
@@ -562,7 +574,7 @@ class SRU(nn.Module):
             ).zero_())
             c0 = [ zeros for i in range(self.depth) ]
         else:
-            # The dimensions of `input` should be: `(num_layers, batch_size, hidden_size * dir_)`.
+            # The dimensions of `c0` should be: `(num_layers, batch_size, hidden_size * dir_)`.
             if c0.dim() != 3:
                 raise ValueError("There must be 3 dimensions for (depth, batch, n_out*dir_)")
             c0 = [ x.squeeze(0) for x in c0.chunk(self.depth, 0) ]
@@ -575,7 +587,7 @@ class SRU(nn.Module):
             lstc.append(c)
 
         if input_packed:
-            prevx = nn.utils.rnn.pack_padded_sequence(prevx, lengths, enforce_sorted=False, batch_first=True)
+            prevx = nn.utils.rnn.pack_padded_sequence(prevx, lengths, enforce_sorted=False)
 
         if return_hidden:
             return prevx, torch.stack(lstc)
--- sru/test_sru.py
+++ sru/test_sru.py
@@ -10,26 +10,31 @@ def test_packed():
     K = 8
     K_out = 11
 
+    print('N', N, 'max_len', max_len, 'K', K, 'K_out', K_out)
+
     torch.manual_seed(123)
     np.random.seed(123)
     lengths = torch.from_numpy(np.random.choice(max_len, N)) + 1
     tensors = [torch.from_numpy(np.random.choice(V, l, replace=True)) for l in lengths.tolist()]
     embedder = nn.Embedding(V, K)
-    tensors = nn.utils.rnn.pad_sequence(tensors, batch_first=True)
+    tensors = nn.utils.rnn.pad_sequence(tensors)
+    print('tensors.size()', tensors.size())
     embedded = embedder(tensors)
-    packed = nn.utils.rnn.pack_padded_sequence(embedded, lengths, batch_first=True, enforce_sorted=False)
+    print('embedded.size()', embedded.size())
+    packed = nn.utils.rnn.pack_padded_sequence(embedded, lengths, batch_first=False, enforce_sorted=False)
     print(isinstance(packed, nn.utils.rnn.PackedSequence))
 
     sru = SRU(K, K_out)
     out1, state = sru(packed)
     out1, lengths1 = nn.utils.rnn.pad_packed_sequence(out1)
+    print('out1.size()', out1.size())
     assert (lengths != lengths1).sum().item() == 0
     print('out1.sum()', out1.sum().item())
 
     # change one of the indexes taht should not be masked out
-    tensors[1, 6] = 3
+    tensors[6, 1] = 3
     embedded = embedder(tensors)
-    packed = nn.utils.rnn.pack_padded_sequence(embedded, lengths, batch_first=True, enforce_sorted=False)
+    packed = nn.utils.rnn.pack_padded_sequence(embedded, lengths, batch_first=False, enforce_sorted=False)
     out2, state = sru(packed)
     out2, lengths2 = nn.utils.rnn.pad_packed_sequence(out2)
     assert (lengths != lengths2).sum().item() == 0
@@ -39,7 +44,7 @@ def test_packed():
     # change one of the indexes taht should be masked out
     tensors[1, 1] = 3
     embedded = embedder(tensors)
-    packed = nn.utils.rnn.pack_padded_sequence(embedded, lengths, batch_first=True, enforce_sorted=False)
+    packed = nn.utils.rnn.pack_padded_sequence(embedded, lengths, batch_first=False, enforce_sorted=False)
     out3, state = sru(packed)
     out3, lengths3 = nn.utils.rnn.pad_packed_sequence(out3)
     assert (lengths != lengths3).sum().item() == 0
