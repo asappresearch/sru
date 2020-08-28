379b44040ba97d3a90e4201ed5f681377ba8fbc5
Tao Lei <taolei@csail.mit.edu>
2020-08-27 23:18:09 -0400

    fix path error

--- test/test.sh
+++ test/test.sh
@@ -6,5 +6,5 @@ cd build
 cmake -DCMAKE_PREFIX_PATH="$(python -c 'import torch; import os.path; print(os.path.join(os.path.dirname(torch.__file__), "share", "cmake"))')" ..
 make -j
 cd ../../../
-sru/csrc/build/example_app test/sru_ts.pt > cpp_out.txt
+sru/csrc/build/example_app sru_ts.pt > cpp_out.txt
 diff cpp_out.txt py_out.txt
