003afb9fdc78368392657e55323e9fca39ed83ed
Tao Lei <taolei@csail.mit.edu>
2020-08-27 22:22:30 -0400

    update test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,6 @@ set -e
 cd sru/csrc/
 mkdir build
 cd build
-cmake -DCMAKE_PREFIX_PATH="$(python -c 'import torch; import os.path;
-    print(os.path.join(os.path.dirname(torch.__file__), "share", "cmake"))')" ..
+cmake -DCMAKE_PREFIX_PATH="$(python -c 'import torch; import os.path; print(os.path.join(os.path.dirname(torch.__file__), "share", "cmake"))')" ..
 make -j
 ./example_app
