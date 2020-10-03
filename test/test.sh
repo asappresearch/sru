2df67f764814d2d8afc6bb39498e641a5eb27953
Hugh Perkins <hp@asapp.com>
2020-10-03 10:12:21 -0400

    first install torch+cpu, in CCIE build, then install sru, then install test requirements

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
+# This script requires to be run as either:
+# - torch is cpu install, or
+# - torch is cuda install, and CUDA toolkit is present
+
 set -e
+set -x
+
 python test/test_ts_cpp.py > py_out.txt
 cd sru/csrc/
+if [[ -d build ]]; then {
+    rm -Rf build
+} fi
 mkdir build
 cd build
 cmake -DCMAKE_PREFIX_PATH="$(python -c 'import torch; import os.path; print(os.path.join(os.path.dirname(torch.__file__), "share", "cmake"))')" ..
