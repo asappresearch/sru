942bf6876a76d6449404e3e75f65e666983dc7b0
Hugh Perkins <hp@asapp.com>
2020-10-08 14:48:56 -0400

    fix artifact

--- test/regression/test_regression.py
+++ test/regression/test_regression.py
@@ -3,15 +3,11 @@ test:
 - running some numbers through two versions of sru, checking they come out the sam
 - save sru in older version, and loading in new version
 
-IMPORTANT:
-
-    You need to run test/regression/build_artifact.sh [SRU VERSION]
-
-for each version you want to test, first
 """
 import torch
 import sru
 import pytest
+import sys
 
 
 EPSILON = 1e-6
@@ -25,6 +21,16 @@ ARTIFACT_DIR = 'test/regression/artifacts'
     ["2.3.5"]
 )
 def test_regression(sru_prev_version):
+    """
+    IMPORTANT:
+
+    You need to run:
+
+        test/regression/build_artifact.sh [SRU VERSION]
+
+    and add the resulting artifact in test/regression/artifacts into github,
+    for each sru_prev_version you want to test
+    """
     torch.manual_seed(2)  # so the model is initialized differently than first stage
 
     artifact_path = f'{ARTIFACT_DIR}/{sru_prev_version}.pt'
@@ -33,8 +39,8 @@ def test_regression(sru_prev_version):
 
     model = sru.SRU(**artifact_dict['sru_kwargs']).eval()
 
-    output_artifact = torch.load(artifact_dict['outputs'])
-    model.load_state_dict(torch.load(artifact_dict['model']))
+    output_artifact = artifact_dict['outputs']
+    model.load_state_dict(artifact_dict['model_state'])
     with torch.no_grad():
         output_current = model(artifact_dict['inputs'])
 
