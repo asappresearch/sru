1e5d2b0b6fb2633417f26d48e61300ae01d84d7e
Tao Lei <tao@asapp.com>
2021-05-18 22:05:10 -0400

    GPU inference in C++

--- sru/csrc/main_test_cpp.cpp
+++ sru/csrc/main_test_cpp.cpp
@@ -6,20 +6,34 @@
 #include <typeinfo>
 
 int main(int argc, const char* argv[]) {
-  if (argc != 2) {
+  if ((argc != 2) && (argc != 3)) {
     std::cerr << "usage: example-app <path-to-exported-script-module>\n";
+    std::cerr << "usage: example-app <path-to-exported-script-module> cuda\n";
     return -1;
   }
 
+  bool use_cuda = (argc == 3) && (strcmp(argv[2], "cuda") == 0);
+
   // Deserialize the ScriptModule from a file using torch::jit::load().
   torch::jit::script::Module module = torch::jit::load(argv[1]);
+  if (use_cuda) {
+    module.to(torch::kCUDA);
+  }
+  else {
+    module.to(torch::kCPU);
+  }
   
   auto num_layers = module.attr("num_layers").toInt();
   auto input_size = module.attr("input_size").toInt();
   auto hidden_size = module.attr("hidden_size").toInt();
   
   std::vector<torch::jit::IValue> inputs;
-  inputs.push_back(torch::ones({3, 2, input_size}));
+  if (use_cuda) {
+    inputs.push_back(torch::ones({3, 2, input_size}).to(torch::kCUDA));
+  }
+  else {
+    inputs.push_back(torch::ones({3, 2, input_size}));
+  }
   auto outputs = module.forward(std::move(inputs));
   auto h = outputs.toTuple()->elements()[0].toTensor();
   auto c = outputs.toTuple()->elements()[1].toTensor();
